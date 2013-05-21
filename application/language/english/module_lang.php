29142bb5b02b3943797045b02884a61dd92ca18f
root <root@ambagape-comp.(none)>
2013-05-21 11:17:05 +0100

    Committing the main code

---
+++ application/language/english/module_lang.php
@@ -0,0 +1,43 @@
+<?php
+$lang['module_home']='Home';
+
+$lang['module_people']='People';
+
+$lang['module_inventory']='Inventory';
+
+$lang['module_settings']='Settings';
+
+$lang['module_help']='Help';
+$lang['module_help_desc']='Get support from trained professionals';
+
+$lang['module_customers']='Customers';
+$lang['module_customers_desc']='Add, Update, Delete, and Search customers';
+
+$lang['module_suppliers']='Suppliers';
+$lang['module_suppliers_desc']='Add, Update, Delete, and Search suppliers';
+
+$lang['module_employees']='Employees';
+$lang['module_employees_desc']='Add, Update, Delete, and Search employees';
+
+$lang['module_sales']='Sales';
+$lang['module_sales_desc']='Process sales and returns';
+
+$lang['module_reports']='Reports';
+$lang['module_reports_desc']='View and generate reports';
+
+$lang['module_items']='Items';
+$lang['module_items_desc']='Add, Update, Delete, and Search items';
+
+$lang['module_config']='Store Config';
+$lang['module_config_desc']='Change the store\'s configuration';
+
+$lang['module_receivings']='Receivings';
+$lang['module_receivings_desc']='Process Purchase orders';
+
+$lang['module_giftcards']='Gift Cards';
+$lang['module_giftcards_desc']='Add, Update, Delete and Search gift cards';
+
+$lang['module_item_kits']='Item Kits';
+$lang['module_item_kits_desc']='Add, Update, Delete and Search Item Kits';
+
+?>
---
+++ application/views/partial/.svn/text-base/footer.php.svn-base
@@ -0,0 +1,7 @@
+</div>
+	</div>
+    <div id="footernote"> <?php echo $this->lang->line('common_you_are_using_poschief'); ?> <?php echo $this->config->item('application_version'); ?>.
+<?php echo $this->lang->line('common_please_visit_my'); ?> <a href="http://www.theictcenter.com" target="_blank"><?php echo $this->lang->line('common_website'); ?></a> <?php echo $this->lang->line('common_learn_about_project'); ?>.</div>
+</div>
+</body>
+</html>
